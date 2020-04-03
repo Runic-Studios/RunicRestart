a605c37871593e3221dfe00e1104ee6197b863e2
Excel619 <kai@enoshlo.com>
2020-04-03 21:45:20 +0300

    Stuffs

--- src/main/java/com/runicrealms/runicrestart/Plugin.java
+++ src/main/java/com/runicrealms/runicrestart/Plugin.java
@@ -4,33 +4,56 @@ import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Plugin extends JavaPlugin {
 
-    public static BukkitTask countdown;
+    public static Set<BukkitTask> tasks = new HashSet<BukkitTask>();
     public static int finish;
     public static int passed;
-
+    public static BukkitTask counter;
+    public static BukkitTask buffer;
 
     private static Plugin instance;
 
     @Override
     public void onEnable() {
         instance = this;
+        this.getConfig().options().copyDefaults(true);
+        this.saveDefaultConfig();
         Bukkit.getPluginCommand("runicrestart").setExecutor(new RestartCommand());
-        Bukkit.getScheduler().runTaskLater(this, new Runnable() {
-            @Override
-            public void run() {
-                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "runicrestart 10");
-            }
-        }, 20L * 60L * 110L);
+        if (this.getConfig().getInt("restart-buffer") >= 0) {
+            buffer = Bukkit.getScheduler().runTaskLater(this, new Runnable() {
+                @Override
+                public void run() {
+                    Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "runicrestart " + (Plugin.getInstance().getConfig().getInt("restart-duration")));
+                    Plugin.buffer = null;
+                }
+            }, 20L * 60L * (this.getConfig().getInt("restart-buffer")));
+        }
     }
 
     @Override
     public void onDisable() {
-        if (countdown != null) {
-            countdown.cancel();
-            countdown = null;
+        for (BukkitTask task : tasks) {
+            if (task.isCancelled() == false){
+                task.cancel();
+            }
         }
+        if (counter != null) {
+            if (!counter.isCancelled()) {
+                counter.cancel();
+            }
+        }
+        if (buffer != null) {
+            if (!buffer.isCancelled()) {
+                buffer.cancel();
+            }
+        }
+        tasks = null;
+        counter = null;
+        buffer = null;
     }
 
     public static Plugin getInstance() {
--- src/main/java/com/runicrealms/runicrestart/RestartCommand.java
+++ src/main/java/com/runicrealms/runicrestart/RestartCommand.java
@@ -5,33 +5,91 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.scheduler.BukkitTask;
+
+import java.util.HashSet;
 
 public class RestartCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (args.length == 1) {
-            if (isInt(args[0])) {
-                Plugin.finish = Integer.parseInt(args[0]);
-                Plugin.passed = 0;
-                Plugin.countdown = Bukkit.getScheduler().runTaskTimer(Plugin.getInstance(), new Runnable() {
-                    @Override
-                    public void run() {
-                        if (Plugin.passed < Plugin.finish) {
-                            if (Plugin.passed % 10 == 0 || Plugin.passed <= 5) {
-                                Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', "&c[&4Restart&c] &2Server restarting in &a" + (Plugin.finish - Plugin.passed) + " &2minutes"));
+        if (sender.isOp()) {
+            if (args.length == 1) {
+                if (isInt(args[0])) {
+                    Plugin.finish = Integer.parseInt(args[0]);
+                    Plugin.passed = 0;
+                    Plugin.counter = Bukkit.getScheduler().runTaskTimer(Plugin.getInstance(), new Runnable() {
+                        @Override
+                        public void run() {
+                            if (Plugin.passed < Plugin.finish) {
+                                if ((Plugin.finish - Plugin.passed) % 5 == 0 || (Plugin.finish - Plugin.passed) <= 5) {
+                                    Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&',
+                                            Plugin.getInstance().getConfig().getString("restart-message-format")
+                                                    .replaceAll("%time%", (Plugin.finish - Plugin.passed) + "")
+                                                    .replaceAll("%unit%", "minute" + (Plugin.finish - Plugin.passed > 1 ? "s" : ""))));
+                                }
+                                if ((Plugin.finish - Plugin.passed == 1)) {
+                                    Plugin.tasks.add(Bukkit.getScheduler().runTaskLater(Plugin.getInstance(), new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&',
+                                                    Plugin.getInstance().getConfig().getString("restart-message-format")
+                                                            .replaceAll("%time%", "30")
+                                                            .replaceAll("%unit%", "seconds")));
+                                        }
+                                    }, 20L * 30L));
+                                    Plugin.tasks.add(Bukkit.getScheduler().runTaskLater(Plugin.getInstance(), new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&',
+                                                    Plugin.getInstance().getConfig().getString("restart-message-format")
+                                                            .replaceAll("%time%", "10")
+                                                            .replaceAll("%unit%", "seconds")));
+                                        }
+                                    }, 20L * 50L));
+                                    for (int i = 0; i < 5; i++) {
+                                        final int current = new Integer(i);
+                                        Plugin.tasks.add(Bukkit.getScheduler().runTaskLater(Plugin.getInstance(), new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&',
+                                                        Plugin.getInstance().getConfig().getString("restart-message-format")
+                                                                .replaceAll("%time%", (5 - current) + "")
+                                                                .replaceAll("%unit%", "second" + ((5 - current) > 1 ? "s" : ""))));
+                                            }
+                                        }, 20L * (55L + current)));
+                                    }
+                                }
+                                Plugin.passed++;
+                            } else {
+                                Bukkit.shutdown();
                             }
-                            Plugin.passed++;
-                        } else {
-                            Bukkit.shutdown();
                         }
+                    }, 0L, 20L * 60L);
+                    sender.sendMessage(ChatColor.GREEN + "Started countdown.");
+                } else if (args[0].equalsIgnoreCase("cancel")) {
+                    if (Plugin.buffer != null) {
+                        Plugin.buffer = null;
+                        Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', Plugin.getInstance().getConfig().getString("restart-cancel-message-format")));
+                    } else if (Plugin.counter != null) {
+                        Plugin.counter.cancel();
+                        for (BukkitTask task : Plugin.tasks) {
+                            task.cancel();
+                        }
+                        Plugin.tasks = new HashSet<BukkitTask>();
+                        Plugin.counter = null;
+                        Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', Plugin.getInstance().getConfig().getString("restart-cancel-message-format")));
+                    } else {
+                        sender.sendMessage(ChatColor.RED + "There isn't a restart to cancel.");
                     }
-                }, 0L, 20L * 60L);
+                } else {
+                    sender.sendMessage(ChatColor.RED + "Command syntax is: /runicrestart <minutes> or /runicrestart cancel");
+                }
             } else {
-                sender.sendMessage(ChatColor.RED + "Command syntax is: /runicrestart <minutes>");
+                sender.sendMessage(ChatColor.RED + "Command syntax is: /runicrestart <minutes> or /runicrestart cancel");
             }
         } else {
-            sender.sendMessage(ChatColor.RED + "Command syntax is: /runicrestart <minutes>");
+            sender.sendMessage(ChatColor.RED + "You do not have permission to do that!");
         }
         return true;
     }
