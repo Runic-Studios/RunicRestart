8c63b7c274526dc2481a2d35cf9fbb1ee00a10c0
Excel619 <kai@enoshlo.com>
2020-04-13 17:59:34 +0300

    Added confimation message in console when plugins mark for saved or loaded

--- src/main/java/com/runicrealms/runicrestart/api/RunicRestartApi.java
+++ src/main/java/com/runicrealms/runicrestart/api/RunicRestartApi.java
@@ -3,10 +3,13 @@ package com.runicrealms.runicrestart.api;
 import com.runicrealms.runicrestart.Plugin;
 import org.bukkit.Bukkit;
 
+import java.util.logging.Level;
+
 public class RunicRestartApi {
 
     public static void markPluginLoaded(String key) {
         Plugin.pluginsToLoad.remove(key);
+        Bukkit.getLogger().log(Level.INFO, "[RunicRestart] " + key + " confirmed startup");
         if (Plugin.pluginsToLoad.size() == 0) {
             if (Plugin.hasWhitelist == false) {
                 Bukkit.setWhitelist(false);
@@ -16,6 +19,7 @@ public class RunicRestartApi {
 
     public static void markPluginSaved(String key) {
         Plugin.pluginsToSave.remove(key);
+        Bukkit.getLogger().log(Level.INFO, "[RunicRestart] " + key + " confirmed shutdown");
         if (Plugin.pluginsToSave.size() == 0) {
             Bukkit.shutdown();
         }
