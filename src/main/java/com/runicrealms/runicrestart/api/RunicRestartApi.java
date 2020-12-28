ba04ca8f1d6ded7398211313385199d73d383987
Excel619 <kai@enoshlo.com>
2020-12-28 12:46:34 -0700

    Fixed bug with custom events trying to fire async

--- src/main/java/com/runicrealms/runicrestart/api/RunicRestartApi.java
+++ src/main/java/com/runicrealms/runicrestart/api/RunicRestartApi.java
@@ -10,11 +10,24 @@ public class RunicRestartApi {
     public static void markPluginLoaded(String key) {
         Plugin.pluginsToLoad.remove(key);
         Bukkit.getLogger().log(Level.INFO, "[RunicRestart] " + key + " confirmed startup");
-        Bukkit.getPluginManager().callEvent(new PluginLoadedEvent(key));
+        if (!Bukkit.isPrimaryThread()) {
+            Bukkit.getScheduler().runTask(Plugin.getInstance(), () -> {
+                Bukkit.getPluginManager().callEvent(new PluginLoadedEvent(key));
+            });
+        } else {
+            Bukkit.getPluginManager().callEvent(new PluginLoadedEvent(key));
+        }
         if (Plugin.pluginsToLoad.size() <= 0) {
             if (!Plugin.hasWhitelist) {
-                Bukkit.getScheduler().runTask(Plugin.getInstance(), () -> Bukkit.setWhitelist(false));
-                Bukkit.getPluginManager().callEvent(new PluginsReadyEvent());
+                if (!Bukkit.isPrimaryThread()) {
+                    Bukkit.getScheduler().runTask(Plugin.getInstance(), () -> {
+                        Bukkit.setWhitelist(false);
+                        Bukkit.getPluginManager().callEvent(new PluginsReadyEvent());
+                    });
+                } else {
+                    Bukkit.setWhitelist(false);
+                    Bukkit.getPluginManager().callEvent(new PluginsReadyEvent());
+                }
             }
         }
     }
@@ -24,7 +37,11 @@ public class RunicRestartApi {
         Bukkit.getLogger().log(Level.INFO, "[RunicRestart] " + key + " confirmed shutdown");
         if (Plugin.pluginsToSave.size() == 0) {
             if (Plugin.shouldShutdown) {
-                Bukkit.getScheduler().runTask(Plugin.getInstance(), Bukkit::shutdown);
+                if (!Bukkit.isPrimaryThread()) {
+                    Bukkit.getScheduler().runTask(Plugin.getInstance(), Bukkit::shutdown);
+                } else {
+                    Bukkit.shutdown();
+                }
             } else {
                 Bukkit.getLogger().log(Level.INFO, "[RunicRestart] All plugins have confirmed shutdown! You are free to use console shutdown.");
             }
